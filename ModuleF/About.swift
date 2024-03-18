986f70f77e60cb283ef68842ebdb9af4a754e443
雷子 <jianlei.chen@lucahealthcare.com>
2024-03-18 18:13:06 +0800

    add about

---
+++ ModuleF/About.swift
@@ -0,0 +1,14 @@
+//
+//  About.swift
+//  ModuleF-repo
+//
+//  Created by 雷子 on 2024/3/18.
+//
+
+import Foundation
+
+public class About {
+    public static func eat() {
+        print("About eat")
+    }
+}
